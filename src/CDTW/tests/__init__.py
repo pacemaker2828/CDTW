79a574de45a56397390d6a6f6d4690ac3c3b69b7
D Jones <pacemaker2828@yahoo.com>
2016-03-06 10:54:07 -0500

    Initial Capstone package structure

---
+++ src/CDTW/tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+CDTW: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2016, Chris Dwayne Todd Willie
+Licensed under MIT
+'''
---
+++ src/CDTW/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+CDTW: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2016, Chris Dwayne Todd Willie
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
